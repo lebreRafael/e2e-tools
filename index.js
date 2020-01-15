c94d73ee9f45b523bf6faf97c90b7e4223ec2974
Rafael Lebre <rafabonalebre@gmail.com>
2020-01-15 15:11:15 -0200

    refactor index as class

--- index.js
+++ index.js
@@ -1,148 +1,79 @@
 /* eslint-disable no-use-before-define */
 
-const path = require('path');
-const fs = require('fs');
-const puppeteer = require('puppeteer');
-
-async function getInputElementHandle (page, identificator, options, inputTag = 'input') {
-  const {tag} = identificator;
-  const {elementHandle} = await getIdentificator(page, identificator);
-  if (tag === 'label') {
-    const inputId = await page.evaluate((labelElement) => labelElement.getAttribute('for'), elementHandle);
-    const inputXPath = '//' + inputTag + '[@id = "' + inputId + '"]';
-    return getElementHandleByXpath(page, inputXPath, options);
+class PuppeteerTools {
+  constructor (page) {
+    this.page = page;
   }
-  return elementHandle;
-}
 
-async function clickElement (page, identificator) {
-  const {elementHandle} = await getIdentificator(page, identificator);
-  await elementHandle.click();
-}
-
-async function doubleClickElement (page, identificator) {
-  const {elementHandle} = await getIdentificator(page, identificator);
-  await elementHandle.click({clickCount: 1});
-  await elementHandle.click({clickCount: 2});
-}
-
-async function getElementHandleByXpath (page, elementPath, options = {}) {
-  const {
-    externalPath,
-    index,
-  } = options;
-  let xPath = (externalPath || '') + elementPath;
-  if (typeof index !== 'undefined') {
-    xPath = '(' + xPath + ')[' + index + ']';
+  async getInputElementHandle (identificator, options, inputTag = 'input') {
+    const {tag} = identificator;
+    const {elementHandle} = await this.getIdentificator(this.page, identificator);
+    if (tag === 'label') {
+      const inputId = await this.page.evaluate((labelElement) => labelElement.getAttribute('for'), elementHandle);
+      const inputXPath = '//' + inputTag + '[@id = "' + inputId + '"]';
+      return this.getElementHandleByXpath(inputXPath, options);
+    }
+    return elementHandle;
   }
-  await page.waitForXPath(xPath);
-  const elements = await page.$x(xPath);
-  if (elements.length > 1 && typeof index === 'undefined') {
-    throw new Error('xPath ' + xPath + ' found more than once and options.index is not specified');
-  }
-  return elements[0];
-}
 
-async function getIdentificator (page, data) {
-  const {
-    isPlaceholder,
-    options,
-    siblingTag,
-    tag,
-    text,
-  } = data;
-  let siblingXPath;
-  if (siblingTag) {
-    siblingXPath = '//following-sibling::' + siblingTag;
-  } else {
-    // Essas tags nunca tem filhos
-    siblingXPath = ['h1', 'h2', 'h3', 'h4', 'legend'].includes(tag) ? '//following-sibling::div' : '';
+  async clickElement (identificator) {
+    const {elementHandle} = await this.getIdentificator(identificator);
+    await elementHandle.click();
   }
-  let {xPath} = options || {};
-  if (!xPath) {
-    if (isPlaceholder) {
-      xPath = '//input[@placeholder = "' + text + '"]' + siblingXPath;
+
+  async doubleClickElement (identificator) {
+    const {elementHandle} = await this.getIdentificator(identificator);
+    await elementHandle.click({clickCount: 1});
+    await elementHandle.click({clickCount: 2});
+  }
+
+  async getElementHandleByXpath (elementPath, options = {}) {
+    const {
+      externalPath,
+      index,
+    } = options;
+    let xPath = (externalPath || '') + elementPath;
+    if (typeof index !== 'undefined') {
+      xPath = '(' + xPath + ')[' + index + ']';
+    }
+    await this.page.waitForXPath(xPath);
+    const elements = await this.page.$x(xPath);
+    if (elements.length > 1 && typeof index === 'undefined') {
+      throw new Error('xPath ' + xPath + ' found more than once and options.index is not specified');
+    }
+    return elements[0];
+  }
+
+  async getIdentificator (data) {
+    const {
+      isPlaceholder,
+      options,
+      siblingTag,
+      tag,
+      text,
+    } = data;
+    let siblingXPath;
+    if (siblingTag) {
+      siblingXPath = '//following-sibling::' + siblingTag;
     } else {
-      xPath = '//' + tag + '[text() = "' + text + '"]' + siblingXPath;
+      // Essas tags nunca tem filhos
+      siblingXPath = ['h1', 'h2', 'h3', 'h4', 'legend'].includes(tag) ? '//following-sibling::div' : '';
     }
-  }
-  const elementHandle = await getElementHandleByXpath(page, xPath, options);
-  return {elementHandle, xPath};
-}
-
-function getModuleName (filePath) {
-  const fileNameParts = filePath.split('/');
-  return fileNameParts[fileNameParts.length - 1].replace('.test.js', '');
-}
-
-async function lauchApplication (moduleName, url) {
-  const args = ['--single-process', '–no-sandbox', '–disable-setuid-sandbox', '--disable-features=Metal'];
-  let launchOptions;
-  if (process.env.NODE_TEST_ENV === 'debug') {
-    launchOptions = {
-      args,
-      headless: false,
-      slowMo: 50,
-    };
-  } else {
-    launchOptions = {
-      args,
-      headless: true,
-      slowMo: 50, // Ainda nao sei pq, mas nao funciona sem isso
-    };
-  }
-  const browser = await puppeteer.launch(launchOptions);
-  const page = await browser.newPage();
-  const consoleMessageFileName = moduleName + '-' + (new Date()).getTime() + '.txt';
-  const consoleMessageFilePath = path.resolve(__dirname, '../consoleMessages/', consoleMessageFileName);
-  fs.writeFileSync(consoleMessageFilePath, '');
-  page.on('console', async (consoleMessage) => {
-    const consoleMessageArgs = consoleMessage.args();
-    let parsedArgs = [];
-    try {
-      parsedArgs = await Promise.all(consoleMessageArgs.map((jsHandle) => {
-        return jsHandle.executionContext().evaluate((obj) => {
-          return JSON.stringify(obj);
-        }, jsHandle);
-      }));
-    } catch (e) {
-      parsedArgs = [
-        'Console message could not be read due to the following error:',
-        e,
-      ];
+    let {xPath} = options || {};
+    if (!xPath) {
+      if (isPlaceholder) {
+        xPath = '//input[@placeholder = "' + text + '"]' + siblingXPath;
+      } else {
+        xPath = '//' + tag + '[text() = "' + text + '"]' + siblingXPath;
+      }
     }
-    const fileData = 'type: ' + consoleMessage.type() + '\n' + consoleMessage.text() + parsedArgs.join('\n') + '\n\n';
-    fs.appendFileSync(consoleMessageFilePath, fileData);
-  });
-  await page.setViewport({height: 600, width: 1280});
-  await page.goto(url);
-  return [browser, page];
+    const elementHandle = await this.getElementHandleByXpath(xPath, options);
+    return {elementHandle, xPath};
+  }
+
+  async waitElement (identificator) {
+    await this.getIdentificator(identificator);
+  }
 }
 
-async function sleep (ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-async function takeScreenshot (page, moduleName) {
-  const screenshotPath = 'screenshots/' + moduleName + '-' + (new Date()).getTime() + '.png';
-  await page.screenshot({path: screenshotPath});
-}
-
-async function waitElement (page, identificator) {
-  await getIdentificator(page, identificator);
-}
-
-module.exports = {
-  getInputElementHandle,
-  clickElement,
-  doubleClickElement,
-  getElementHandleByXpath,
-  getIdentificator,
-  getModuleName,
-  lauchApplication,
-  sleep,
-  takeScreenshot,
-  waitElement,
-};
+module.exports = (page) => new PuppeteerTools(page);
