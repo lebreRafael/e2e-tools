b9a3e2e8bc2f829e51fda261e29fc8e73301a7de
Rafael Lebre <rafabonalebre@gmail.com>
2020-01-24 14:37:50 -0200

    comment unnused code

--- form.js
+++ form.js
@@ -5,22 +5,22 @@ class Form {
     this.input = new Input(page);
   }
 
-  /**
-   *
-   * @param formData
-   * @returns {Promise<void>}
-   */
-  async check (formData) {
-    // Pra usar forEach e async/await é bem mais chato.
-    for (let i = 0; i < formData.length; i++) {
-      /* eslint-disable no-await-in-loop */ // Não quero rodar essas promises em paralelo
-      const data = formData[i];
-      if (!data.skipCheck) {
-        await this.input.check(data);
-      }
-      /* eslint-enable no-await-in-loop */
-    }
-  }
+  // /**
+  //  *
+  //  * @param formData
+  //  * @returns {Promise<void>}
+  //  */
+  // async check (formData) {
+  //   // Pra usar forEach e async/await é bem mais chato.
+  //   for (let i = 0; i < formData.length; i++) {
+  //     /* eslint-disable no-await-in-loop */ // Não quero rodar essas promises em paralelo
+  //     const data = formData[i];
+  //     if (!data.skipCheck) {
+  //       await this.input.check(data);
+  //     }
+  //     /* eslint-enable no-await-in-loop */
+  //   }
+  // }
 
   /**
    *
